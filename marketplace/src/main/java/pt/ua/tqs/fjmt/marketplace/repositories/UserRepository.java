3741a2ec147b2ea934cfbdf1f846e69ee23797d5
joao-p-marques <joao.marques.contacto@gmail.com>
2020-05-19 11:44:13 +0100

    add simple user repo

---
+++ marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package pt.ua.tqs.fjmt.marketplace.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pt.ua.tqs.fjmt.marketplace.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public List<User> findAll();
+
+}
