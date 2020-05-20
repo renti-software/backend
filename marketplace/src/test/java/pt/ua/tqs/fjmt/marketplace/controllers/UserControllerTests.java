5ada481af9084514d68fb55368b148212e28e62c
joao-p-marques <joao.marques.contacto@gmail.com>
2020-05-20 17:56:04 +0100

    create user controller tests

--- marketplace/src/test/java/pt/ua/tqs/fjmt/marketplace/controllers/UserControllerTests.java
+++ marketplace/src/test/java/pt/ua/tqs/fjmt/marketplace/controllers/UserControllerTests.java
@@ -20,6 +20,7 @@ import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -38,28 +39,30 @@ public class UserControllerTests {
         mockMvc.perform(get("/users/")).andExpect(status().isOk());
     }
 
-    @Test 
-    @DisplayName("After inserting user API should return it")
-    public void whenUserInserted_thenReturnsUser() {
-
-    }
-
     @Test
     @DisplayName("Should return 404 when user not found")
-    public void whenUserDoesNotExist_thenReturnsNotFound() {
-
+    public void whenUserDoesNotExist_thenReturnsNotFound() throws Exception {
+        mockMvc.perform(get("users/1462")).andExpect(status().isNotFound());
     }
 
     @Test
     @DisplayName("Inserting user should return 200")
-    public void whenCorrectInsertion_thenReturnsOk() {
-
+    public void whenCorrectInsertion_thenReturnsOk() throws Exception {
+        mockMvc.perform(post("/users/", new User())).andExpect(status().isOk());
     }
 
     @Test
     @DisplayName("Inserting bad user should return 400")
-    public void whenIncorrectInsertion_thenReturnsBadRequest() {
+    public void whenIncorrectInsertion_thenReturnsBadRequest() throws Exception {
+        mockMvc.perform(post("/users/", "")).andExpect(status().isBadRequest());
+    }
 
+    @Test 
+    @DisplayName("After inserting user API should return it")
+    public void whenUserInserted_thenReturnsUser() throws Exception {
+        User u = new User("alex", "", null, "");
+        mockMvc.perform(post("/users/", u)).andExpect(status().isOk());
+        mockMvc.perform(post("/users/", u)).andExpect(status().isOk()).andExpect(content().string(containsString("alex")));
     }
 
 }
