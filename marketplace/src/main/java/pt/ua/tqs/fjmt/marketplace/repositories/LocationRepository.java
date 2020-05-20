641512be096f8c0d2d178449e5fdf25447166062
joao-p-marques <joao.marques.contacto@gmail.com>
2020-05-20 14:07:12 +0100

    add 1st version of repository

---
+++ marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/repositories/LocationRepository.java
@@ -0,0 +1,15 @@
+package pt.ua.tqs.fjmt.marketplace.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pt.ua.tqs.fjmt.marketplace.entities.Location;
+
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Long> {
+
+    public List<Location> findAll();
+
+}
