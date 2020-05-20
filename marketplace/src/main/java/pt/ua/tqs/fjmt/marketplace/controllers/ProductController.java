8db6701976da4e3f65fde44632b9cd7997c04028
franciscojesus <franciscojesus@ua.pt>
2020-05-20 16:20:28 +0100

    Product Repository almost complete (missing search by location) and tested, product Controller complete

--- marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/controllers/ProductController.java
+++ marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/controllers/ProductController.java
@@ -1,17 +1,15 @@
 package pt.ua.tqs.fjmt.marketplace.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pt.ua.tqs.fjmt.marketplace.entities.Product;
 import pt.ua.tqs.fjmt.marketplace.repositories.ProductRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 @CrossOrigin(origins = "*")
 public class ProductController {
 
@@ -20,4 +18,30 @@ public class ProductController {
 
     @GetMapping("/")
     public List<Product> findAll() {return productRepository.findAll();}
+
+    @GetMapping("/{id}")
+    public Optional<Product> findbyId(@PathVariable("id") Long id){
+        return productRepository.findById(id);
+    }
+
+    @GetMapping("/name/{name}")
+    public List<Product> findbyName(@PathVariable("name") String name){
+        return productRepository.findByName(name);
+    }
+
+    @GetMapping("/category/{category}")
+    public List<Product> findbyCategory(@PathVariable("category") String category){
+        return productRepository.findByCategory(category);
+    }
+
+    @GetMapping("/price/{price}")
+    public List<Product> findbyPrice(@PathVariable("price") float price){
+        return productRepository.findByPrice(price);
+    }
+
+    @PostMapping("/")
+    public void addProduct(Product product){
+        productRepository.save(product);
+    }
+
 }
--- marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/entities/Product.java
+++ marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/entities/Product.java
@@ -22,7 +22,7 @@ public class Product {
     @OneToOne(cascade = CascadeType.ALL)
     private Location location;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.EAGER, optional = false, cascade=CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
