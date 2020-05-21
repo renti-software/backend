2f7ca9a62538c15ac0cca57c2a55a3a4b9231710
joao-p-marques <joao.marques.contacto@gmail.com>
2020-05-21 11:55:39 +0100

    1st version of LocationController

---
+++ marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/controllers/LocationController.java
@@ -0,0 +1,38 @@
+package pt.ua.tqs.fjmt.marketplace.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.*;
+import pt.ua.tqs.fjmt.marketplace.MarketplaceApplication;
+import pt.ua.tqs.fjmt.marketplace.entities.Location;
+import pt.ua.tqs.fjmt.marketplace.repositories.LocationRepository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/locations")
+@CrossOrigin(origins = "*")
+public class LocationController {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    @GetMapping("")
+    public List<Location> findAll() {
+        return locationRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Location> findbyId(@PathVariable("id") Long id){
+        return locationRepository.findById(id);
+    }
+
+    @PostMapping("")
+    public void addLocation(@RequestBody Location location){
+        locationRepository.save(location);
+    }
+
+}
