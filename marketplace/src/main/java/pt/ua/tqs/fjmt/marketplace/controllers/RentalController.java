2798dc3aee315ac2065534c0695ef2b6e1877350
joao-p-marques <joao.marques.contacto@gmail.com>
2020-05-22 13:55:44 +0100

    feat(rentals): start implementing to satisfy tests. change filetering

--- marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/controllers/RentalController.java
+++ marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/controllers/RentalController.java
@@ -1,12 +1,16 @@
 package pt.ua.tqs.fjmt.marketplace.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import pt.ua.tqs.fjmt.marketplace.entities.Product;
 import pt.ua.tqs.fjmt.marketplace.entities.Rental;
 import pt.ua.tqs.fjmt.marketplace.entities.User;
 import pt.ua.tqs.fjmt.marketplace.repositories.RentalRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,32 +18,54 @@ import java.util.Optional;
 @RequestMapping("/rentals")
 @CrossOrigin(origins = "*")
 public class RentalController {
+
     @Autowired
     RentalRepository rentalRepository;
 
     @GetMapping("")
-    public List<Rental> findAll(){
-        return rentalRepository.findAll();
+    public List<Rental> filter(@RequestParam(required = false) User renter,
+                               @RequestParam(required = false) Product product
+                            ) {
+
+        List<Rental> found = new ArrayList<>();
+        if (renter != null) {
+            found = rentalRepository.findByRenter(renter);
+            if (found.size() == 0) {
+                throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Rentals not found for this Renter"
+                );
+            }
+        }
+        else if (product != null) {
+            found = rentalRepository.findByProduct(product);
+            if (found.size() == 0) {
+                throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Rentals not found for this Product"
+                );
+            }
+        }
+        else {
+            found = rentalRepository.findAll();
+        }
+        return found;
     }
 
     @GetMapping("/{id}")
     public Optional<Rental> findById(@PathVariable("id") Long id){
-        return rentalRepository.findById(id);
-    }
-
-    @GetMapping("/renter/{user}")
-    public List<Rental> findByRenter(@PathVariable("user") User user){
-        return rentalRepository.findByRenter(user);
-    }
-
-    @GetMapping("/product/{product}")
-    public List<Rental> findByProduct(@PathVariable("product") Product product){
-        return rentalRepository.findByProduct(product);
+        Optional<Rental> found = rentalRepository.findById(id);
+        if (found.isPresent()) {
+            return found;
+        }
+        else {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Rental not found"
+            );
+        }
     }
 
     @PostMapping("")
-    public String addRental(Rental rental){
+    public Long addRental(Rental rental){
         rentalRepository.save(rental);
-        return "New rental added";
+        return rental.getId();
     }
 }
