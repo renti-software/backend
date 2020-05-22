923cacfd02bfcb43c068030d7732cfd33436aec9
joao-p-marques <joao.marques.contacto@gmail.com>
2020-05-22 12:34:57 +0100

    fix rental according to standards

--- marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/entities/Rental.java
+++ marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/entities/Rental.java
@@ -16,20 +16,17 @@ public class Rental {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.EAGER, optional = false, cascade=CascadeType.ALL)
-    @JoinColumn(name = "user_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(fetch = FetchType.EAGER, cascade=CascadeType.MERGE)
     private User renter;
 
-    @ManyToOne(fetch = FetchType.EAGER, optional = false, cascade=CascadeType.ALL)
-    @JoinColumn(name = "product_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(fetch = FetchType.EAGER, cascade=CascadeType.MERGE)
     private Product product;
 
     private LocalDate startDate;
     private LocalDate endDate;
 
-    public Rental() {this(null, null, null, null);}
+    public Rental() {
+    }
 
     public Rental(User renter, Product product, LocalDate startDate, LocalDate endDate) {
         this.renter = renter;
@@ -38,35 +35,4 @@ public class Rental {
         this.endDate = endDate;
     }
 
-    public User getRenter() {
-        return renter;
-    }
-
-    public void setRenter(User renter) {
-        this.renter = renter;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
 }
