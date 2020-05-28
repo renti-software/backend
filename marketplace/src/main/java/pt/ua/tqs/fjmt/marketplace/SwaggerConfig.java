fbcca94ea25d51f333dd5214f42b45ce5b7a0b07
chicoUA <franciscojesus@ua.pt>
2020-05-28 03:43:10 -0700

    swagger still not working

--- marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/SwaggerConfig.java
+++ marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/SwaggerConfig.java
@@ -3,8 +3,12 @@ package pt.ua.tqs.fjmt.marketplace;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,11 +19,11 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(Predicates.not(RequestHandlerSelectors.
-                        basePackage("pt.ua.tqs.fjmt.marketplace.controllers")))
-                .paths(PathSelectors.any()).build();
-
+                .apis(RequestHandlerSelectors.basePackage("pt.ua.tqs.fjmt.marketplace.controllers"))
+                .paths(PathSelectors.any())
+                .build();
     }
+
 
 
 }
