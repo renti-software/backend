9f4821262d0666f6cacc3cc99d18ecb38a37c201
chicoUA <franciscojesus@ua.pt>
2020-05-28 03:06:25 -0700

    trying new method for swagger

--- marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/MarketplaceApplication.java
+++ marketplace/src/main/java/pt/ua/tqs/fjmt/marketplace/MarketplaceApplication.java
@@ -11,21 +11,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@ComponentScan
-@EnableSwagger2
 public class MarketplaceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MarketplaceApplication.class, args);
 	}
-
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage(" pt.ua.tqs.fjmt.marketplace.controllers"))  // // Generate API of EndPoints which is available inside defined package
-				.paths(PathSelectors.any())   // for all EndPoints
-				.build();             // create object
-	}
-
 }
